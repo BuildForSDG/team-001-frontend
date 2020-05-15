540be5cabd40df0a888d18d8d586c059e91d3cc5
Victor Okpon <vikzy2rhyme@yahoo.com>
2020-05-15 10:18:17 +0100

    installed CRA installed bootstrap popper.js

---
+++ betalife-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-center my-2">
+        <h1 className="text-primary">Betalife</h1>
+        <p className="text-secondary"> This app matches users with existing opportunities in skill training </p>
+        <p> -------- Edit this UI ------- </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
