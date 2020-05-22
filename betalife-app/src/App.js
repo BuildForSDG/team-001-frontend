eb97e360a3ba85e031f5c188ae76dc86829bbc37
Victor Okpon <vikzy2rhyme@yahoo.com>
2020-05-22 08:59:09 +0100

    Added Material Design Bootstrap component library. Added Bootstarp 4 component library. Desined Intro page. Designed UI for Login. Designed UI for Sign up. Installed library for date picker.

--- betalife-app/src/App.js
+++ betalife-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Login from './components/Login'
+import Intro from './components/Intro';
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        <Login/>
+        <Intro/>
       </div>
     );
   }
